05e9b9ac622889856a82314e5001d0c9db08eeb0
Daniel Song <xdtsong@gmail.com>
2022-03-09 13:44:31 -0800

    Add API Gateway module

--- api_gateway/variables.tf
+++ api_gateway/variables.tf
@@ -1,16 +1,10 @@
 variable "region" {}
 variable "account_id" {}
 variable "function_name" {}
-variable "swagger_api_file" {}
-variable "invoke_arn" {}
-variable "lambda_version" {}
 
-variable "domain_name" {
-  default = "NOTSET"
-}
-
-variable "domain_zone_id" {
-  default = "NOTSET"
+variable "lambda_invoke_arn" {
+  description = "Lambda ARN to be invoked by this API Gateway"
+  type = string
 }
 
 variable "certificate_arn" {
@@ -19,22 +13,6 @@ variable "certificate_arn" {
 
 variable "stage_name" {
   default = "api"
-}
-
-variable "api_version" {
-  default = "v1"
-}
-
-variable "authorizer_uri" {
-  default = "NOT_SET"
-}
-
-variable "api_caller_ps_name" {
-  default = "/env/apiCaller"
-}
-
-variable "lambda_authorizer_name" {
-  default = "NOT_SET"
 }
 
 variable "cache_enabled" {
@@ -59,21 +37,4 @@ variable "api_gateway_endpoint_configuration_types" {
 
 variable "api_gateway_security_policy" {
   default = "TLS_1_2"
-}
-
-locals {
-  pub_gateway_name = "${var.function_name}-public"
-  gateway_name     = var.authorizer_uri == "NOT_SET" ? var.function_name : local.pub_gateway_name
-}
-
-locals {
-  authorizer_name           = "${var.function_name}-authorizer"
-  formatted_arn             = "\"${var.invoke_arn}\""
-  formatted_auth_uri        = "\"${var.authorizer_uri}\""
-  formatted_gateway_name    = "\"${local.gateway_name}\""
-  formatted_title_swagger   = replace(file(var.swagger_api_file), "/(\"title\"\\s*:\\s*)\"(\\S*)\"/", "$1${local.formatted_gateway_name}")
-  formatted_uri_swagger     = replace(local.formatted_title_swagger, "/(\"uri\"\\s*:\\s*)\"(arn:aws:apigateway:\\S*)\"/", "$1${local.formatted_arn}")
-  formatted_authuri_swagger = replace(local.formatted_uri_swagger, "/(\"authorizerUri\"\\s*:\\s*)\"(arn:aws:apigateway:\\S*)\"/", "$1${local.formatted_auth_uri}")
-  invoke_statement_id       = uuid() - "${var.lambda_version}"
-  auth_invoke_statement_id  = "${uuid()}-${var.lambda_version}"
 }
