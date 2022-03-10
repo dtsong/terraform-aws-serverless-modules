da6e0793ad8580f19e81ba6aa597deddb004180f
Daniel Song <xdtsong@gmail.com>
2022-03-09 18:34:31 -0800

    Add script to automatically update module README files

---
+++ update-docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -a MODULE_FOLDERS=( "api_gateway" "iam/lambda" "iam/lambda_api_gateway" "lambda" "rds" "secrets_manager" "security_group" "vpc")
+
+if terraform-docs -h 2&> /dev/null; then
+  echo "terraform-docs is installed - proceeding..."
+else
+  echo "aborting - terraform-docs is not installed"
+  exit 1
+fi
+
+for i in "${MODULE_FOLDERS[@]}"
+do
+    terraform-docs markdown table --output-file README.md --output-mode inject "./${i}"
+done
